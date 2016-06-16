feb1b313a8accb32bbd9b8e58e5851623e82eaf0
jellybeanjohnny <mamerige@icloud.com>
2016-06-16 10:07:38 -0400

    Wired up UI and added logic to pass tests

--- OOPCardsSimpleViews/FISCardSwitcherViewController.m
+++ OOPCardsSimpleViews/FISCardSwitcherViewController.m
@@ -4,6 +4,12 @@
 
 @interface FISCardSwitcherViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *topLeftLabel;
+@property (weak, nonatomic) IBOutlet UILabel *middleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *bottomRightLabel;
+
+
+
 @end
 
 @implementation FISCardSwitcherViewController
@@ -21,5 +27,34 @@
     self.tenOfHearts = [[FISCard alloc] initWithSuit:@"♥️"
                                                 rank:@"10"];
 }
+
+- (void)_updateLabelsForCard:(FISCard *)card
+{
+	self.topLeftLabel.text = card.cardLabel;
+	self.middleLabel.text = card.cardLabel;
+	self.bottomRightLabel.text = card.cardLabel;
+}
+
+- (IBAction)threeOfSpadesTapped:(id)sender
+{
+	[self _updateLabelsForCard:self.threeOfSpades];
+}
+
+- (IBAction)fourOfClubsTapped:(id)sender
+{
+	[self _updateLabelsForCard:self.fourOfClubs];
+}
+
+- (IBAction)eightOfDiamondsTapped:(id)sender
+{
+	[self _updateLabelsForCard:self.eightOfDiamonds];
+}
+
+- (IBAction)tenOfHeartsTapped:(id)sender
+{
+	[self _updateLabelsForCard:self.tenOfHearts];
+}
+
+
 
 @end
